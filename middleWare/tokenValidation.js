a040131030188db139ec1f49ce25d4c906716abb
Marcestordeur <marcstord@gmail.com>
2024-01-16 18:03:51 -0300

    verifytoken

---
+++ middleWare/tokenValidation.js
@@ -0,0 +1,20 @@
+const jwt =  require('jsonwebtoken')
+require('dotenv').config();
+
+const verifytoken = (request, response, next) => {
+  let token = request.header('Authorization')
+
+  if(token.startsWith('Bearer ')){
+    token = token.slice(7, token.lenght).trimLeft()
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return response.status(400).json(error)
+    }
+
+    next()
+  })
+}
+
+module.exports = {verifytoken}
