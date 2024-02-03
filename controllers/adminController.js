46ca61e59867ef440f293b2fca1baa840545999c
Marcelo <marcstord@gmail.com>
2024-02-03 16:26:34 -0300

    error fix

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -15,24 +15,24 @@ const createProduct = async (request, response) => {
       img,
       description,
       price,
-    })
+    });
     //save the new product in the data base
-    await NewProduct.save()
-    response.status(200).json({message: 'se creo con exito'})
+    await NewProduct.save();
+    response.status(200).json({message: 'se creo con exito'});
   } catch (error) {
-    response.status(400).json({message: 'no se pudo crear.'})
+    response.status(400).json({message: 'no se pudo crear.'});
   }
-}
+};
 
 const getAllProducts = async () => {
   try {
     //we use find({}) to get all productos from the server
-    const products = await Products.find({})
-    response.status(200).json(products)
+    const products = await Products.find({});
+    response.status(200).json(products);
   } catch (error) {
-    response.status(500).json({ message: 'No se pudieron encontrar los productos' })
+    response.status(500).json({ message: 'No se pudieron encontrar los productos' });
   }
-}
+};
 
 const createCancha = async () => {
   const { Title, description, id, img, Date} = request.body
@@ -44,32 +44,32 @@ const createCancha = async () => {
       img,
       description,
       Date
-    })
+    });
     //then we save the new cancha
-    await NewCancha.save()
-    response.status(200).json({message: 'se creo con exito'})
+    await NewCancha.save();
+    response.status(200).json({message: 'se creo con exito'});
   } catch (error) {
-    response.status(400).json({message: 'no se pudo crear.'})
+    response.status(400).json({message: 'no se pudo crear.'});
   }
-}
+};
 
 const getAllCancha = async () => {
   try {
     // we use find({}) to get all from canchas in the data base
-    const canchas = await Cancha.find({})
-    response.status(200).json(canchas)
+    const canchas = await Cancha.find({});
+    response.status(200).json(canchas);
   } catch (error) {
-    response.status(500).json({ message: 'No se pudieron encontrar los productos' })
+    response.status(500).json({ message: 'No se pudieron encontrar los productos' });
   }
-}
+};
 
 const getAllUsers = async() => {
   try {
     // we use find({}) to get all from users in the data base
-    const users = await User.find({})
-    response.status(200).json(users)
+    const users = await User.find({});
+    response.status(200).json(users);
   } catch (error) {
-    response.status(500).json({ message: 'No se pudieron encontrar usuarios' })
+    response.status(500).json({ message: 'No se pudieron encontrar usuarios' });
   }
 };
 
@@ -85,25 +85,40 @@ const UserDisable = async (request, response) => {
   } catch (error) {
     
   }
+};
+
+const DeleteUser = async (request, response) => {
+  const {id} = request.body;
+  try {
+    const user = await User.findOne({_id: id});
+    if(!user){
+      return response.status(400).json({message:'no se encontro el usuario'});
+    }
+
+    await User.deleteOne({_id:id})
+
+  } catch (error) {
+    response.status(400).json({message:'no se pudo realizar la accion'})
+  }
 }
 
 const DeleteProducts = async (request, response) => {
   //we use an input where the user can write a number
-  const { id } = request.body
+  const { id } = request.body;
   try {
     // the number from the front is a id, then use these id to find the product in the data base
     const product = await Products.findOne({_id: id});
     // we check if the product exist
     if(!product){
       return response.status(400).json({message: 'no existe el producto'})
-    }
+    };
     // and if it exist we delete the product by id
-    await Products.deleteOne({_id: id})
+    await Products.deleteOne({_id: id});
 
   } catch (error) {
-    response.status(400).json({message: 'no se pudo realizar la accion'})
+    response.status(400).json({message: 'no se pudo realizar la accion'});
   }
-}
+};
 
 const DeleteCanchas = async (request, response) => {
   //we use an input where the user can write a number
@@ -122,4 +137,4 @@ const DeleteCanchas = async (request, response) => {
   }
 }
 
-module.exports = { createProduct, createCancha, getAllProducts, getAllCancha, getAllUsers, DeleteCanchas, DeleteProducts}
+module.exports = { createProduct, createCancha, getAllProducts, getAllCancha, getAllUsers, DeleteCanchas, DeleteProducts, DeleteUser}
