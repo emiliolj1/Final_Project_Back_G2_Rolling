ab67034c95919cab428aa4ca36febbe216b0bf0e
Emilio Luna Jandar <lunaemilio269@gmail.com>
2024-02-04 16:36:37 -0300

    Fix login error

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -3,52 +3,106 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
 require('dotenv').config();
 
-const loginUser = async (req, res) => {
-  try {
-    const { email, password } = req.body;
+// const loginUser = async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
-    if(!user) return res.status(400).json({ message: 'usuario no existente'});
+//     const user = await User.findOne({ email});
+//     if(!user) return res.status(400).json({ message: 'usuario no existente'});
     
-    const isMatch = bcrypt.compareSync(password, user.password);
-    if(!isMatch) return res.status(400).json({message: 'contraseña erronea'});
+//     const isMatch = bcrypt.compareSync(password, user.password);
+//     if(!isMatch) return res.status(400).json({message: 'contraseña erronea'});
 
-    const accessToken = jwt.sign({ 
-      name: user.name, 
-      lastName: user.lastName,
-      email: user.email, 
-      role: user.role
-    },
-      process.env.ACCESS_TOKEN_SECRET, 
-    {
-      expiresIn: 30 //por defecto en segundos.
-    });
+//     const accessToken = jwt.sign({ 
+//       name: user.name, 
+//       email: user.email, 
+//       role: user.role
+//     },
+//       process.env.ACCESS_TOKEN_SECRET, 
+//     {
+//       expiresIn: 30 //por defecto en segundos.
+//     });
 
-    const refreshToken = jwt.sign({
-      name: user.name, 
-      lastName: user.lastName,
-      email: user.email, 
-      role: user.role
-    },
-    process.env.REFRESH_TOKEN_SECRET,
-    {
-      expiresIn: '1d'
-    });
+//     const refreshToken = jwt.sign({
+//       name: user.name, 
+//       email: user.email, 
+//       role: user.role
+//     },
+//     process.env.REFRESH_TOKEN_SECRET,
+//     {
+//       expiresIn: '1d'
+//     });
 
-    //guardado de refreshToken
+//     user.refreshToken = refreshToken;
+//     await user.save();
+
+//     res.cookie('refreshToken', refreshToken, { 
+//       httpOnly: true, 
+//       sameSite: 'None', 
+//       secure: true,
+//       maxAge: 24*60*60*1000
+//     })
+//     res.status(200).json({ accessToken });
+//   } catch (error) {
+//     res.status(500).json({message: 'todo mal' })
+//   }
+// }
+
+const loginUser =  async (request, response) => {
+  try {
+    const {email, password} = request.body
+    
+    const user = await User.findOne({ email })
+    // searching for the user if it exists 
+    if(!user){
+      return response.status(400).json({message: 'usuario no existe'})
+    }
+
+    // the user exist, their password is the same?
+    const isMatch = bcrypt.compareSync(password, user.password)
+    if(!isMatch){
+      return response.status(400).json({ message: 'contraseña invalida' })
+    }
+
+    const accessToken = jwt.sign(
+      { 
+        email: user.email,
+        Name: user.Name,
+        role: user.role
+      }, 
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: '120s'
+      }
+    )
+
+    const refreshToken = jwt.sign(
+      {
+        id: user._id,
+        email: user.email,
+        Name: user.Name,
+        role: user.role
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn:'1d'
+      }
+    )
+
+    // saving the refreshtoken in the user
     user.refreshToken = refreshToken;
-    await user.save();
+    await user.save()
 
-    res.cookie('refreshToken', refreshToken, { 
+    response.cookie('refreshToken', refreshToken, { 
       httpOnly: true, 
       sameSite: 'None', 
-      secure: true,
-      maxAge: 24*60*60*1000
+      secure: true, 
+      maxAge: 24 * 60 * 60 * 1000 
     })
-    res.status(200).json({ accessToken });
+    response.status(200).json({ accessToken, message: 'Succesfull User Logged'})
   } catch (error) {
-    res.status(500).json({message: error })
+    response.status(500).json({ message: error.message })
   }
-}
+};
 
 module.exports = { loginUser };
--- controllers/userController.js
+++ controllers/userController.js
@@ -4,79 +4,26 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const addUser = async (request, response) => {
-  // in a constant we put everything we want to control
-  const { Name, email, password, role} = request.body
   try {
-    // we use the schema
+    const { Name, email, password, role, refreshToken} = request.body
     const newUser = new User({
       Name,
       email,
       password,
-      role
+      role,
+      refreshToken
     })
     // use the tool bcrypt to generate an encrypted password
     const saltRounds = 10;
     const salt = bcrypt.genSaltSync(saltRounds);
     const hash = bcrypt.hashSync(password, salt);
+    
     newUser.password = hash
 
     await newUser.save()
     response.status(200).json({ message: 'el usuario fue creado con exito' })
   } catch (error) {
     response.status(400).json({ message: 'no se pudo crear el usuario' }) 
-  }
-};
-
-const loginUser =  async (request, response) => {
-  try {
-    const {email, password} = request.body
-    const user = await User.findOne({ email })
-
-    // searching for the user if it exists 
-    if(!user){
-      return response.status(400).json({message: 'usuario no existe'})
-    }
-
-    // the user exist, their password is the same?
-    const isMatch = bcrypt.compareSync(password, user.password)
-    if(!isMatch){
-      return response.status(400).json({ message: 'contraseña invalida' })
-    }
-
-    const accessToken = jwt.sign(
-      { 
-        id: user._id,
-        email: user.email,
-        Name: user.Name,
-        role: user.role
-      }, 
-      process.env.ACCESS_TOKEN_SECRET,
-      {
-        expiresIn: '120s'
-      }
-    )
-
-    const refreshToken = jwt.sign(
-      {
-        id: user._id,
-        email: user.email,
-        Name: user.Name,
-        role: user.role
-      },
-      process.env.REFRESH_TOKEN_SECRET,
-      {
-        expiresIn:'1d'
-      }
-    )
-
-    // saving the refreshtoken in the user
-    user.refreshToken = refreshToken;
-    await user.save()
-
-    response.cookie('refreshToken', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 })
-    response.status(200).json({ accessToken })
-  } catch (error) {
-    response.status(500).json({ message: error.message })
   }
 };
 
@@ -108,4 +55,4 @@ const changePassword = async (request, response) => {
 }
 
 
-module.exports = { addUser, loginUser, changePassword };
+module.exports = { addUser, changePassword };
