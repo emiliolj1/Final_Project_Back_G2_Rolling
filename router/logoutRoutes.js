ee9f0acc87843eadf43e079103bad0cf7e7b089e
Marcestordeur <marcstord@gmail.com>
2024-02-06 01:38:25 -0300

    fixing git commit -m fixing

---
+++ router/logoutRoutes.js
@@ -0,0 +1,7 @@
+const { Router } = require('express');
+const route = Router();
+const { handleLogout } = require('../controllers/logoutController')
+
+route.get('/logout', handleLogout);
+
+module.exports = route;
--- server/server.js
+++ server/server.js
@@ -36,6 +36,14 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser())
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
+
 //Routes
 app.use('/', addUser);
 app.use('/', loginUser);
