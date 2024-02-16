3d6cbeebb99702942de9a86bdedb5064bd71ffdd
Marcestordeur <marcstord@gmail.com>
2024-02-15 23:17:23 -0300

    fix comments

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,7 @@
 const {model, Schema} = require('mongoose')
 
 const user =  new Schema({
-  name: {type: String, require: true, minlength: 5, maxlength: 25}, // EL nombre debe ser unico? No hay 2 personas con el mismo nombre? xD
+  name: {type: String, require: true, minlength: 5, maxlength: 25}, 
   email: {type: String, unique: true, require: true, minlength: 5, maxlength:30},
   password: {type: String, require: true, minlength: 5},
   role: {type: String, enum:['client', 'admin', 'Master'], default:'client'},
