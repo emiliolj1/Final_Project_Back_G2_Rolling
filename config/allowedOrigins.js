c2b8f5d1f20a1e9699f36ee627602880469f1c8e
Emilio Luna Jandar <lunaemilio269@gmail.com>
2024-03-05 23:11:39 -0300

    Added route in config/allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
-  'http://localhost:5173'
+  'http://localhost:5173',
+  'https://salefulbo.netlify.app'
 ]
 
 module.exports = { allowedOrigins }
